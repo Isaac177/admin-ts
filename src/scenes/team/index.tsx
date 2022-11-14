5815a6d5c044bf92aca992b0e3b098be31bdb8e0
Isaac177 <isaacjogues177@gmail.com>
2022-11-14 23:50:21 +0600

    Set Team

--- src/scenes/team/index.tsx
+++ src/scenes/team/index.tsx
@@ -3,34 +3,104 @@ import {Box, Typography, useTheme} from "@mui/material";
 import {tokens} from "../../theme/theme";
 import {DataGrid} from "@mui/x-data-grid";
 import {mockDataTeam} from "../../data/mockData";
+import Header from "../../components/Header";
+import AdminPanelSettingsOutlinedIcon from '@mui/icons-material/AdminPanelSettingsOutlined';
+import SecurityOutlinedIcon from '@mui/icons-material/SecurityOutlined';
+import LockOpenOutlinedIcon from '@mui/icons-material/LockOpenOutlined';
 
 
 const Team: React.FC = () => {
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
 
-    const columns = [
-        {field: 'id', headerName: 'ID', width: 70},
-        {field: 'firstName', headerName: 'First name', width: 130},
-        {field: 'lastName', headerName: 'Last name', width: 130},
-        {field: 'age', headerName: 'Age', type: 'number', width: 90},
-        {field: 'fullName', headerName: 'Full name', description: 'This column has a value getter and is not sortable.', sortable: false, width: 160},
+    const columns: any = [
+        { field: "id", headerName: "ID" },
+        {
+            field: "name",
+            headerName: "Name",
+            flex: 1,
+            cellClassName: "name-column--cell",
+        },
+        {
+            field: "age",
+            headerName: "Age",
+            type: "number",
+            headerAlign: "left",
+            align: "left",
+        },
+        {
+            field: "phone",
+            headerName: "Phone Number",
+            flex: 1,
+        },
+        {
+            field: "email",
+            headerName: "Email",
+            flex: 1,
+        },
+        {
+            field: "accessLevel",
+            headerName: "Access Level",
+            flex: 1,
+
+            renderCell:(access: any) => {
+                return (
+                    <Box
+                        sx={{
+                            width: '60%',
+                            margin: '0 auto',
+                            padding:'5px',
+                            display:'flex',
+                            justifyContent: 'center',
+                            backgroundColor: access.value === 'Admin' ? colors.blueAccent[700] : colors.greenAccent[700],
+                            borderRadius: '4px',
+                    }}>
+                        {access === "admin" && <AdminPanelSettingsOutlinedIcon />}
+                        {access === "manager" && <SecurityOutlinedIcon />}
+                        {access === "user" && <LockOpenOutlinedIcon />}
+                        <Typography color={colors.grey[100]} sx={{ ml: "5px" }}>
+                            {access}
+                        </Typography>
+                    </Box>
+                );
+            },
+        },
     ];
 
+    // @ts-ignore
     return (
-        <Box>
-            <Box display="flex" justifyContent="space-between" alignItems="center">
-                <Typography
-                    variant="h2"
-                    color={colors.grey[100]}
-                    fontWeight="bold"
-                    sx={{ m: "0 0 5px 0" }}
-                >
-                    TEAM
-                </Typography>
-            </Box>
-            <Box height="500px" width="100%">
-                <DataGrid rows={mockDataTeam} columns={columns} pageSize={5} checkboxSelection />
+        <Box m="20px">
+            <Header title="TEAM" subtitle="Managing the Team Members" />
+            <Box
+                m="40px 0 0 0"
+                height="75vh"
+                sx={{
+                    "& .MuiDataGrid-root": {
+                        border: "none",
+                    },
+                    "& .MuiDataGrid-cell": {
+                        borderBottom: "none",
+                    },
+                    "& .name-column--cell": {
+                        color: colors.greenAccent[300],
+                    },
+                    "& .MuiDataGrid-columnHeaders": {
+                        backgroundColor: colors.blueAccent[700],
+                        borderBottom: "none",
+                    },
+                    "& .MuiDataGrid-virtualScroller": {
+                        backgroundColor: colors.primary[400],
+                    },
+                    "& .MuiDataGrid-footerContainer": {
+                        borderTop: "none",
+                        backgroundColor: colors.blueAccent[700],
+                    },
+                    "& .MuiCheckbox-root": {
+                        color: `${colors.greenAccent[200]} !important`,
+                    },
+                }}
+            >
+                <DataGrid checkboxSelection rows={mockDataTeam}  columns={columns}/>
             </Box>
         </Box>
     );
